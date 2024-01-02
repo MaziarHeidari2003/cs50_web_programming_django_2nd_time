8bbccc735e934d986d7e1bc75d2bf8438ca174fa
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-01-02 13:28:44 -0800

    late night commit newyear app

--- lecture3/hello/urls.py
+++ lecture3/hello/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("", views.index, name="index")
+  path("", views.index, name="index"),
+  path("maziar",views.maziar, name="maziar"),
+  path("messi", views.messi , name="messi"),
+  path("<str:name>",views.greet, name = "greet")
 ]
--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -4,4 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request): # the http request that the user made in order to acces the web server
-  return HttpResponse("Hello world")
+  return render(request, "hello/index.html")
+
+def maziar(request):
+  return HttpResponse('Hello maziar')
+
+def messi(request):
+  return HttpResponse('Hello messi')
+
+def greet(request, name):
+  return render(request, "hello/greet.html", {
+    "name": name.capitalize()
+  })
