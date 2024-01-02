4e05fb5df126b21bd9f16adf3678cd8c396c9ce3
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-01-02 11:22:56 -0800

    hello app finishing

--- lecture3/lecture3/urls.py
+++ lecture3/lecture3/urls.py
@@ -1,3 +1,5 @@
+
+
 """
 URL configuration for lecture3 project.
 
@@ -15,8 +17,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', include('hello.urls'))
 ]
