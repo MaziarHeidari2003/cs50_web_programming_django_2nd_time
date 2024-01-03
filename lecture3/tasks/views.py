335c04e88dbbaf1760b9dc1b656e3ff8385f9439
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-01-03 00:22:08 -0800

    there is something wrong here

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
 
-tasks= ['fpp', 'bar', 'baz']
+class NewTaskForm(forms.Form):
+  task = forms.CharField(label = 'New Task')
+  priority = forms.IntegerField(label='pririty', min_value=1, max_value= 5)
 
 def index(request):
+  if "tasks" not in request.session:
+    request.session["tasks"] = []
+
   return render(request, "tasks/index.html", {
-    "tasks": tasks
+    "tasks": request.session['tasks']
   })
 
 
 def add(request):
-  return render(request, "tasks/add.html")
+  if request.method == 'POST':
+    form = NewTaskForm(request.POST)
+    if form.is_valid():
+      task = form.cleaned_data['task']
+      request.session["tasks"] += [task]
+      return HttpResponseRedirect(reverse("tasks:index"))
+    else:
+      return render(request, "tasks/add.html", {
+        "form": form
+      })  
+
+  return render(request, "tasks/add.html", {
+    "form": NewTaskForm()
+  })
