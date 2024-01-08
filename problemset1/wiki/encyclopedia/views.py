ca180e24a43680b97be1c2fa2c48a6a66650aa1c
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-01-08 03:46:26 -0800

    error_entry_pages

--- problemset1/wiki/encyclopedia/views.py
+++ problemset1/wiki/encyclopedia/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
-
+import markdown
 from . import util
 
 
+def convert_md_to_html(title):
+    content = util.get_entry(title)
+    markdowner =markdown.Markdown()
+    if content == None:
+        return None
+    else:
+       return markdowner.convert(content)
+
+
 def index(request):
+    entries = util.list_entries()
+    css_file = util.get_entry("CSS")
+    coffee = util.get_entry("coffee")
+    
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+
     })
 
+def entry(request, title):
+    html_content = convert_md_to_html(title)
+    if html_content == None:
+        return render(request, "encyclopedia/error.html")
+    else:
+        return render(request, "encyclopedia/entry.html")
