f045062aeb337e75752d33b510fa09c0094c6495
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-01-08 04:49:57 -0800

    the search is done

--- problemset1/wiki/encyclopedia/views.py
+++ problemset1/wiki/encyclopedia/views.py
@@ -25,6 +25,33 @@ def index(request):
 def entry(request, title):
     html_content = convert_md_to_html(title)
     if html_content == None:
-        return render(request, "encyclopedia/error.html")
+        return render(request, "encyclopedia/error.html" , {
+            "message": "This entry does not exist"
+        })
     else:
-        return render(request, "encyclopedia/entry.html")
+        return render(request, "encyclopedia/entry.html", {
+            "title":title,
+            "content": html_content
+        })
+    
+def search(request):
+    if request.method == 'POST':
+        entry_search = request.POST['q']
+        html_content = convert_md_to_html(entry_search)
+        if html_content is not None:
+            return render(request, "encyclopedia/entry.html", {
+                "title": entry_search,
+                "content": html_content
+         })    
+        else:
+            all_entries = util.list_entries()
+            recommendation = []
+            for entry in all_entries:
+                if entry_search.lower() in entry.lower():
+                    recommendation.append(entry)
+            return render(request, "encyclopedia/search.html", {
+                "recommendation": recommendation
+            })        
+
+            
+           
