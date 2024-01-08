06064813e034474d0c773ee2994f458e87314d85
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-01-08 07:25:30 -0800

    almost done, before starting the random page

--- problemset1/wiki/encyclopedia/views.py
+++ problemset1/wiki/encyclopedia/views.py
@@ -54,4 +54,42 @@ def search(request):
             })        
 
             
-           
+def new_page(request):
+    if request.method == "GET":
+        return render(request, "encyclopedia/new.html")
+    else:
+        title = request.POST['title']
+        content = request.POST['content']
+        titleExist = util.get_entry(title)
+        if titleExist is not None:
+            return render(request, "encyclopedia/error.html", {
+                "message": "Entery page already exists"
+            })
+        else:
+            util.save_entry(title,content)
+            html_content = convert_md_to_html(title)
+            return render(request, "encyclopedia/entry.html", {
+                "title": title,
+                "content": html_content
+            })
+
+def edit(request):
+    if request.method == 'POST':
+        title = request.POST['entry_title']
+        content = util.get_entry(title)
+        return render(request, "encyclopedia/edit.html", {
+            "title": title,
+            "content": content
+        })        
+
+
+def save_edit(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        content = request.POST['content']
+        util.save_entry(title, content)
+        html_content = convert_md_to_html(title)
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": html_content
+        })        
