bd002d6c6ecd0075b6c6fb6de27efb62550a61ca
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-01-10 01:34:00 -0800

    lets make tasks per user

--- Lecture3_2nd_time/lecture3/tasks/views.py
+++ Lecture3_2nd_time/lecture3/tasks/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse 
+tasks = []
 
-tasks = ['foo','bazz','bar']
+class NewTaskForm(forms.Form):
+  task = forms.CharField(label="New Task")
+  priority = forms.IntegerField(label="Priority", min_value=1 , max_value=4)
+
+
 # Create your views here.
 
 def index(request):
@@ -9,4 +17,18 @@ def index(request):
   })
 
 def add(request):
-  return render(request, "tasks/add.html")
+  if request.method == "POST":
+    form = NewTaskForm(request.POST)
+    if form.is_valid():
+      task = form.cleaned_data['task']
+      tasks.append(task)
+      return HttpResponseRedirect(reverse('tasks:index'))
+    else:
+      return render(request, "tasks/add.html", {
+        "form": form
+      })  
+
+
+  return render(request, "tasks/add.html", {
+    "form": NewTaskForm()
+  })
