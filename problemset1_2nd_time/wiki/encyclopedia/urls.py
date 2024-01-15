5faf6c672e219633b633a7b7141b979c4dbd53e1
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-01-15 12:34:34 -0800

    before creating the random page

--- problemset1_2nd_time/wiki/encyclopedia/urls.py
+++ problemset1_2nd_time/wiki/encyclopedia/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
-    path("search/",views.search, name="search")
+    path("search/",views.search, name="search"),
+    path("new/", views.new_page, name="new_page"),
+    path("edit/",views.edit, name="edit"),
+    path("save_edit", views.save_edit, name="save_edit")
 ]
--- problemset1_2nd_time/wiki/encyclopedia/views.py
+++ problemset1_2nd_time/wiki/encyclopedia/views.py
@@ -53,3 +53,46 @@ def search(request):
             return render(request,"encyclopedia/search.html",{
                 "recommendation": recommendation
             } )        
+        
+
+def new_page(request):
+    if request.method == "GET":
+        return render(request, "encyclopedia/new.html")      
+    else:
+        title = request.POST['title']
+        content = request.POST['content']
+        title_exist = util.get_entry(title)
+        if title_exist is not None :
+            return render(request,"encyclopedia/error.html", {
+                "message": "Entry page already exists"
+            })
+        else:
+            util.save_entry(title,content)
+            html_content = convert_md_to_html(title)
+            return render(request, "encyclopedia/entry.html", {
+                "title": title,
+                "content": html_content
+            })
+
+
+def edit(request):
+    if request.method == 'POST':
+        title = request.POST['entry_tag']
+        content = util.get_entry(title)
+        return render(request, "encyclopedia/edit.html", {
+            "title":title,
+            "content": content
+        })
+
+
+def save_edit(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        content = request.POST['content']
+        html_content = convert_md_to_html(title)
+        util.save_entry(title,content)
+        return render(request, "encyclopedia/entry.html", {
+                "title": title,
+                "content": html_content
+            })
+        
